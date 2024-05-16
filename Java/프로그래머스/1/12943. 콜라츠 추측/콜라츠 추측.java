f2dc65c2c45f24b085c745531dba9e23ed909849
ggumi030 <130031828+ggumi030@users.noreply.github.com>
2024-05-16 09:15:17 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.02 ms, Memory: 75.5 MB -BaekjoonHub

---
+++ Java/프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int solution(int num) {
+        int count = 0;
+        long number = num;
+        
+        while(number != 1){
+            
+            if(number % 2 == 0){
+                number = number/2;
+            }else{
+                number = number * 3 + 1;
+            }
+            count++;
+            
+            if(count>=500){
+                return -1;
+            }
+        }
+        
+        return count;
+    }
+}
