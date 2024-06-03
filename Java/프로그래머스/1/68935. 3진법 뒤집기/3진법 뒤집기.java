8668e84ea8f8ebd4c032c628898136e05cca098f
ggumi030 <130031828+ggumi030@users.noreply.github.com>
2024-06-03 09:53:48 +0900

    [level 1] Title: 3진법 뒤집기, Time: 11.46 ms, Memory: 78.9 MB -BaekjoonHub

--- Java/프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
+++ Java/프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -1,21 +1,22 @@
-import java.util.*;
-
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        Stack<Integer> result = new Stack<Integer>();
-       
-        while(n != 0){
-            result.push(n%3);
-            n = n/3;
+
+        String str = "";
+
+        while(n>0)
+        {
+            int r = n%3;
+            n = n / 3;
+            str = r + str;
         }
-        int size = result.size();
-        
-        for(int i =0; i < size; i++){
-            answer += result.pop() * Math.pow(3,i); 
-            System.out.println(answer);
+        System.out.println(str);
+
+        for(int i=0; i<str.length(); i++)
+        {
+            answer += Math.pow(3,i) * (str.charAt(i) - '0');
         }
-    
+
         return answer;
     }
 }
