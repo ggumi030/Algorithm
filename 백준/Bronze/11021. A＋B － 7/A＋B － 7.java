c5dd91d87db1a97c39e85a5612c96fe73aafead6
ggumi030 <130031828+ggumi030@users.noreply.github.com>
2025-05-14 10:51:39 +0900

    [Bronze V] Title: A+B - 7, Time: 108 ms, Memory: 14244 KB -BaekjoonHub

--- 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
@@ -5,16 +5,23 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
+        StringBuilder sb;
         
         int T = Integer.parseInt(br.readLine());
         
         for(int i = 0; i < T; i++) {
-            
+            sb = new StringBuilder();
             st = new StringTokenizer(br.readLine());
+            
             int firstNum = Integer.parseInt(st.nextToken());
             int secondNum = Integer.parseInt(st.nextToken());
             
-            System.out.println("Case #" + (i+1) + ": " + (firstNum + secondNum));
+            sb.append("Case #");
+            sb.append(i + 1);
+            sb.append(": ");
+            sb.append(firstNum + secondNum);
+            
+            System.out.println(sb.toString());
         }
         
     }
